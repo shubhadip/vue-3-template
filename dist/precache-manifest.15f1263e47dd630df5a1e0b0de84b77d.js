8dfdfc38ea7f3afd79debec3d56df83a30dfad78
shubhadip <shubhadip.maity@gmail.com>
2020-11-25 07:46:27 +0530

    chore: addition of dist files [skip ci]

---
+++ dist/precache-manifest.15f1263e47dd630df5a1e0b0de84b77d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2de8f673411445de2e",
+    "url": "/css/app.ca936927.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ad8c6c36f3eda646424e6889f77b496a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f84874349cbafd5b3612",
+    "url": "/js/about.7bed70fa.js"
+  },
+  {
+    "revision": "8f2de8f673411445de2e",
+    "url": "/js/app.28a37306.js"
+  },
+  {
+    "revision": "1d72cc2e022776701597",
+    "url": "/js/cc.a7aa79a6.js"
+  },
+  {
+    "revision": "8bb198ec037be230ab5e",
+    "url": "/js/chunk-vendors.532624f3.js"
+  },
+  {
+    "revision": "629d2e97eaa8f17577f3",
+    "url": "/js/cprop.e1f423a8.js"
+  },
+  {
+    "revision": "5ba804bbc1f68a79a8c9",
+    "url": "/js/network.db71f603.js"
+  },
+  {
+    "revision": "37734180de3f47b55231",
+    "url": "/js/service.74ac956e.js"
+  },
+  {
+    "revision": "a57e9488812bd25c2cb8",
+    "url": "/js/test.d5f4fc8d.js"
+  },
+  {
+    "revision": "2c7d601344bf3d786cffd1fbcb7bf301",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
